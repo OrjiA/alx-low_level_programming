9b1e10ccf85581ed61ecb4638380d3be94c628b3
OrjiA <anitaorji0987@gmail.com>
2023-10-13 17:05:50 -0700

    main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_h
+#define MAIN.H
+
+#include <stdio.h>
+
+void positive_or_negative(int i_);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print _remaining_days(int month, int day, int year);
+
+#endif
