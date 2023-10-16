472e441bbb4e3f25ee1ccf62b9306b5d681e8387
OrjiA <anitaorji0987@gmail.com>
2023-10-15 19:26:19 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,19 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i < '8'; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i + '1'; j < '9'; j++)
+		for (j = i + '1'; j <= '9'; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-				putchar((k % 10) + '0');
+				if ((j != i) != k)
+				{
+				putchar(i);
+				putchar(j);
+				putchar(k);
 
-				if (i == '7' && j == '8' && k == '9')
+				if (i == '7' && j == '8')
 					continue;
 				putchar(',');
 				putchar(' ');
@@ -29,5 +31,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return (0);
+	return (0);	
 }
